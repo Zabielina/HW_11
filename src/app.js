9ff261d91de2e22d3e160151c5f130b35904f61e
Zabielina <lizazabielina@gmail.com>
2024-08-18 14:30:22 -0200

    HW_13

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 import express from 'express';
 import path from 'path';
 import exphbs from 'express-handlebars';
-import userRoutes from './routes/routes.js'; 
+import userRoutes from './routes/routes.js';
+import https from 'https';     
+import fs from 'fs';           
 
 const app = express();
 const PORT = 3000;
+
+
+const privateKey = fs.readFileSync('src/server.key', 'utf8');
+const certificate = fs.readFileSync('src/server.cert', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 
 const hbs = exphbs.create({
@@ -19,16 +26,16 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join('src', 'views'));
 
-
 app.use(express.static(path.join('src', 'public')));
-
 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 
+app.use(userRoutes);
 
-app.use(userRoutes); 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const server = https.createServer(credentials, app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
 });
